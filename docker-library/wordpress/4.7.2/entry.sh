dffa90b90ccae8f9f99559a87ae32e470cead2f3
Jeffrey Fan <jiangzhu.fan@outlook.com>
2017-02-23 15:01:38 +0000

    add redis

--- docker-library/wordpress/4.7.2/entry.sh
+++ docker-library/wordpress/4.7.2/entry.sh
@@ -3,4 +3,6 @@
 service mysql start
 mysql -u root -e "create user 'phpmyadmin'@'localhost' identified by 'P@sSw0rd'; grant all on *.* to 'phpmyadmin'@'localhost' with grant option; create user 'phpmyadmin'@'127.0.0.1' identified by 'P@sSw0rd'; grant all on *.* to 'phpmyadmin'@'127.0.0.1' with grant option; flush privileges;"
 
+redis-server --daemonize yes
+
 httpd -DFOREGROUND
