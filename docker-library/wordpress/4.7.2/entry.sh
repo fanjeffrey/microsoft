3fb11fae4fa720a0aecd42192ee62c006ba0de9d
Jeffrey Fan <jiangzhu.fan@outlook.com>
2017-02-24 13:09:51 +0000

    parameterize installation of WordPress

--- docker-library/wordpress/4.7.2/entry.sh
+++ docker-library/wordpress/4.7.2/entry.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
-service mysql start
-mysql -u root -e "create user 'phpmyadmin'@'localhost' identified by 'P@sSw0rd'; grant all on *.* to 'phpmyadmin'@'localhost' with grant option; create user 'phpmyadmin'@'127.0.0.1' identified by 'P@sSw0rd'; grant all on *.* to 'phpmyadmin'@'127.0.0.1' with grant option; flush privileges;"
+set_var_if_null(){
+	local varname="$1"
+	if [ ! "${!varname:-}" ]; then
+		export "$varname"="$2"
+	fi
+}
 
-redis-server --daemonize yes
+echo "WordPress home : $WORDPRESS_HOME"
 
+# if WordPress is not installed/configured
+if [ ! -f "$WORDPRESS_HOME/wp-config.php" ]; then
+	# set vars for WordPress if not provided
+	set_var_if_null "WORDPRESS_DB_HOST" "127.0.0.1"
+	set_var_if_null "WORDPRESS_DB_NAME" "wordpress"
+	set_var_if_null "WORDPRESS_DB_USER" "wordpress"
+	set_var_if_null "WORDPRESS_DB_PASSWORD" "MS173m_QN"
+	set_var_if_null "WORDPRESS_DB_TABLE_NAME_PREFIX" "wp_"
+
+	# check if use native MariaDB
+	# if yes, we allow users to use native phpMyAdmin and native Redis server
+	if [ $WORDPRESS_DB_HOST == "127.0.0.1" ]; then
+		# set vars for phpMyAdmin if not provided
+		set_var_if_null 'PHPMYADMIN_USER' 'phpmyadmin'
+		set_var_if_null 'PHPMYADMIN_PASSWORD' 'MS173m_QN'
+
+		# start native database 
+		service mysql start
+
+		# create database and databse user for WordPress
+		mysql -u root -e "create database $WORDPRESS_DB_NAME; grant all on $WORDPRESS_DB_NAME.* to '$WORDPRESS_DB_USER'@'127.0.0.1' identified by '$WORDPRESS_DB_PASSWORD'; flush privileges;"
+
+		# create database user for phpMyAdmin
+		mysql -u root -e "create user '$PHPMYADMIN_USER'@'127.0.0.1' identified by '$PHPMYADMIN_PASSWORD'; grant all on *.* to '$PHPMYADMIN_USER'@'127.0.0.1' with grant option; flush privileges;"	
+		
+		# start native Redis server
+		redis-server --daemonize yes
+	fi
+	
+	# update wp-config.php
+	cp "$WORDPRESS_HOME/wp-config.php.microsoft" "$WORDPRESS_HOME/wp-config.php"
+	sed -i "s/connectstr_dbhost = '';/connectstr_dbhost = '$WORDPRESS_DB_HOST';/" "$WORDPRESS_HOME/wp-config.php"
+	sed -i "s/connectstr_dbname = '';/connectstr_dbname = '$WORDPRESS_DB_NAME';/" "$WORDPRESS_HOME/wp-config.php"
+	sed -i "s/connectstr_dbusername = '';/connectstr_dbusername = '$WORDPRESS_DB_USER';/" "$WORDPRESS_HOME/wp-config.php"
+	sed -i "s/connectstr_dbpassword = '';/connectstr_dbpassword = '$WORDPRESS_DB_PASSWORD';/" "$WORDPRESS_HOME/wp-config.php"
+	sed -i "s/table_prefix  = 'wp_';/table_prefix  = '$WORDPRESS_DB_TABLE_NAME_PREFIX';/" "$WORDPRESS_HOME/wp-config.php"
+	chown www-data:www-data "$WORDPRESS_HOME/wp-config.php"
+fi
+
+# start Apache HTTPD
 httpd -DFOREGROUND
