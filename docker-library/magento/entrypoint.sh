113d8beb5c3df9c2522b8306a4f7746932c8b393
Jeffrey Fan <jiangzhu.fan@outlook.com>
2017-03-09 16:37:09 +0800

    magento: bug fixes

--- docker-library/magento/entrypoint.sh
+++ docker-library/magento/entrypoint.sh
@@ -7,35 +7,35 @@ set_var_if_null(){
 	fi
 }
 
-test ! -d $WORDPRESS_HOME_AZURE && echo "INFO: WordPress site home on Azure: $WORDPRESS_HOME_AZURE not found!"
+test ! -d $MAGENTO_HOME_AZURE && echo "INFO: Magento site home on Azure: $MAGENTO_HOME_AZURE not found!"
 
-# That wp-config.php doesn't exist means WordPress is not installed/configured yet.
-if [ ! -f "$WORDPRESS_HOME/wp-config.php" ]; then
+# That wp-config.php doesn't exist means Magento is not installed/configured yet.
+if [ ! -f "$MAGENTO_HOME/wp-config.php" ]; then
 	# create wp-config.php
-	mv $WORDPRESS_SOURCE/wp-config.php.microsoft $WORDPRESS_SOURCE/wp-config.php
-	# set WordPress vars if they're not declared or unset
-        set_var_if_null "WORDPRESS_DB_HOST" "127.0.0.1"
-        set_var_if_null "WORDPRESS_DB_NAME" "wordpress"
-        set_var_if_null "WORDPRESS_DB_USERNAME" "wordpress"
-        set_var_if_null "WORDPRESS_DB_PASSWORD" "MS173m_QN"
-        set_var_if_null "WORDPRESS_DB_TABLE_NAME_PREFIX" "wp_"
+	mv $MAGENTO_SOURCE/wp-config.php.microsoft $MAGENTO_SOURCE/wp-config.php
+	# set Magento vars if they're not declared or unset
+        set_var_if_null "MAGENTO_DB_HOST" "127.0.0.1"
+        set_var_if_null "MAGENTO_DB_NAME" "magento"
+        set_var_if_null "MAGENTO_DB_USERNAME" "magento"
+        set_var_if_null "MAGENTO_DB_PASSWORD" "MS173m_QN"
+        set_var_if_null "MAGENTO_DB_TABLE_NAME_PREFIX" "wp_"
         # replace {'localhost',,} with '127.0.0.1'
-        if [ "${WORDPRESS_DB_HOST,,}" = "localhost" ]; then
-                export WORDPRESS_DB_HOST="127.0.0.1"
-                echo "Replace localhost with 127.0.0.1 ... $WORDPRESS_DB_HOST"
+        if [ "${MAGENTO_DB_HOST,,}" = "localhost" ]; then
+                export MAGENTO_DB_HOST="127.0.0.1"
+                echo "Replace localhost with 127.0.0.1 ... $MAGENTO_DB_HOST"
         fi
        	# update wp-config.php with the vars above 
-        sed -i "s/connectstr_dbhost = '';/connectstr_dbhost = '$WORDPRESS_DB_HOST';/" "$WORDPRESS_SOURCE/wp-config.php"
-        sed -i "s/connectstr_dbname = '';/connectstr_dbname = '$WORDPRESS_DB_NAME';/" "$WORDPRESS_SOURCE/wp-config.php"
-        sed -i "s/connectstr_dbusername = '';/connectstr_dbusername = '$WORDPRESS_DB_USERNAME';/" "$WORDPRESS_SOURCE/wp-config.php"
-        sed -i "s/connectstr_dbpassword = '';/connectstr_dbpassword = '$WORDPRESS_DB_PASSWORD';/" "$WORDPRESS_SOURCE/wp-config.php"
-        sed -i "s/table_prefix  = 'wp_';/table_prefix  = '$WORDPRESS_DB_TABLE_NAME_PREFIX';/" "$WORDPRESS_SOURCE/wp-config.php"
+        sed -i "s/connectstr_dbhost = '';/connectstr_dbhost = '$MAGENTO_DB_HOST';/" "$MAGENTO_SOURCE/wp-config.php"
+        sed -i "s/connectstr_dbname = '';/connectstr_dbname = '$MAGENTO_DB_NAME';/" "$MAGENTO_SOURCE/wp-config.php"
+        sed -i "s/connectstr_dbusername = '';/connectstr_dbusername = '$MAGENTO_DB_USERNAME';/" "$MAGENTO_SOURCE/wp-config.php"
+        sed -i "s/connectstr_dbpassword = '';/connectstr_dbpassword = '$MAGENTO_DB_PASSWORD';/" "$MAGENTO_SOURCE/wp-config.php"
+        sed -i "s/table_prefix  = 'wp_';/table_prefix  = '$MAGENTO_DB_TABLE_NAME_PREFIX';/" "$MAGENTO_SOURCE/wp-config.php"
 
 	# Because Azure Web App on Linux uses /home/site/wwwroot,
 	# so if /home/site/wwwroot doesn't exist, 
 	# we think the container is not running on Auzre.
-	if [ ! -d "$WORDPRESS_HOME_AZURE" ]; then
-        	rm -rf $WORDPRESS_HOME && mkdir -p $WORDPRESS_HOME
+	if [ ! -d "$MAGENTO_HOME_AZURE" ]; then
+        	rm -rf $MAGENTO_HOME && mkdir -p $MAGENTO_HOME
 		rm -rf $PHPMYADMIN_HOME && mkdir -p $PHPMYADMIN_HOME
 		rm -rf $MARIADB_DATA_DIR && mkdir -p $MARIADB_DATA_DIR
 		rm -rf $HTTPD_LOG_DIR && mkdir -p $HTTPD_LOG_DIR
@@ -46,7 +46,7 @@ if [ ! -f "$WORDPRESS_HOME/wp-config.php" ]; then
 		test ! -d $HTTPD_LOG_DIR_AZURE && mkdir -p $HTTPD_LOG_DIR_AZURE
 		test ! -d $MARIADB_LOG_DIR_AZURE && mkdir -p $MARIADB_LOG_DIR_AZURE
 	fi
-        cp -R $WORDPRESS_SOURCE/* $WORDPRESS_HOME/ && chown -R www-data:www-data $WORDPRESS_HOME/ && rm -rf $WORDPRESS_SOURCE
+        cp -R $MAGENTO_SOURCE/* $MAGENTO_HOME/ && chown -R www-data:www-data $MAGENTO_HOME/ && rm -rf $MAGENTO_SOURCE
         cp -R $PHPMYADMIN_SOURCE/* $PHPMYADMIN_HOME/ && chown -R www-data:www-data $PHPMYADMIN_HOME/ && rm -rf $PHPMYADMIN_SOURCE
         cp -R $MARIADB_DATA_DIR_TEMP/* $MARIADB_DATA_DIR/ && chown -R mysql:mysql $MARIADB_DATA_DIR/ && rm -rf $MARIADB_DATA_DIR_TEMP
 	chown -R www-data:www-data $HTTPD_LOG_DIR/
@@ -54,21 +54,21 @@ if [ ! -f "$WORDPRESS_HOME/wp-config.php" ]; then
 
 	# check if use native MariaDB
 	# if yes, we allow users to use native phpMyAdmin and native Redis server
-	if [ $WORDPRESS_DB_HOST = "127.0.0.1" ]; then
+	if [ $MAGENTO_DB_HOST = "127.0.0.1" ]; then
 		# set vars for phpMyAdmin if not provided
 		set_var_if_null 'PHPMYADMIN_USERNAME' 'phpmyadmin'
 		set_var_if_null 'PHPMYADMIN_PASSWORD' 'MS173m_QN'
 		# start native database 
 		service mysql start
-		# create database and databse user for WordPress
-		mysql -u root -e "create database $WORDPRESS_DB_NAME; grant all on $WORDPRESS_DB_NAME.* to '$WORDPRESS_DB_USERNAME'@'127.0.0.1' identified by '$WORDPRESS_DB_PASSWORD'; flush privileges;"
+		# create database and databse user for Magento
+		mysql -u root -e "create database $MAGENTO_DB_NAME; grant all on $MAGENTO_DB_NAME.* to '$MAGENTO_DB_USERNAME'@'127.0.0.1' identified by '$MAGENTO_DB_PASSWORD'; flush privileges;"
 		# create database user for phpMyAdmin
 		mysql -u root -e "create user '$PHPMYADMIN_USERNAME'@'127.0.0.1' identified by '$PHPMYADMIN_PASSWORD'; grant all on *.* to '$PHPMYADMIN_USERNAME'@'127.0.0.1' with grant option; flush privileges;"	
 		# start native Redis server
 		redis-server --daemonize yes
 	fi
 else
-	if grep "connectstr_dbhost = '127.0.0.1'" "$WORDPRESS_HOME/wp-config.php"; then
+	if grep "connectstr_dbhost = '127.0.0.1'" "$MAGENTO_HOME/wp-config.php"; then
 		service mysql start
 		redis-server --daemonize yes
 	fi
