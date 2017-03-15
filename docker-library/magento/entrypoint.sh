cc5f71bedfb3d8e1c17d6c1b00c878f0349add13
Jeffrey Fan <jiangzhu.fan@outlook.com>
2017-03-15 13:06:12 +0800

    fix bugs in switching to production mode

--- docker-library/magento/entrypoint.sh
+++ docker-library/magento/entrypoint.sh
@@ -7,35 +7,36 @@ set_var_if_null(){
 	fi
 }
 
-test ! -d $MAGENTO_HOME_AZURE && echo "INFO: Magento site home on Azure: $MAGENTO_HOME_AZURE not found!"
+# That app/etc/env.php doesn't exist means Magento is not installed/configured yet.
+if [ ! -f "$MAGENTO_HOME/app/etc/env.php" ]; then
+	echo "env.app not found. installing magento ..."
 
-# That wp-config.php doesn't exist means Magento is not installed/configured yet.
-if [ ! -f "$MAGENTO_HOME/wp-config.php" ]; then
-	# create wp-config.php
-	mv $MAGENTO_SOURCE/wp-config.php.microsoft $MAGENTO_SOURCE/wp-config.php
-	# set Magento vars if they're not declared or unset
-        set_var_if_null "MAGENTO_DB_HOST" "127.0.0.1"
-        set_var_if_null "MAGENTO_DB_NAME" "magento"
-        set_var_if_null "MAGENTO_DB_USERNAME" "magento"
-        set_var_if_null "MAGENTO_DB_PASSWORD" "MS173m_QN"
-        set_var_if_null "MAGENTO_DB_TABLE_NAME_PREFIX" "wp_"
-        # replace {'localhost',,} with '127.0.0.1'
-        if [ "${MAGENTO_DB_HOST,,}" = "localhost" ]; then
-                export MAGENTO_DB_HOST="127.0.0.1"
-                echo "Replace localhost with 127.0.0.1 ... $MAGENTO_DB_HOST"
-        fi
-       	# update wp-config.php with the vars above 
-        sed -i "s/connectstr_dbhost = '';/connectstr_dbhost = '$MAGENTO_DB_HOST';/" "$MAGENTO_SOURCE/wp-config.php"
-        sed -i "s/connectstr_dbname = '';/connectstr_dbname = '$MAGENTO_DB_NAME';/" "$MAGENTO_SOURCE/wp-config.php"
-        sed -i "s/connectstr_dbusername = '';/connectstr_dbusername = '$MAGENTO_DB_USERNAME';/" "$MAGENTO_SOURCE/wp-config.php"
-        sed -i "s/connectstr_dbpassword = '';/connectstr_dbpassword = '$MAGENTO_DB_PASSWORD';/" "$MAGENTO_SOURCE/wp-config.php"
-        sed -i "s/table_prefix  = 'wp_';/table_prefix  = '$MAGENTO_DB_TABLE_NAME_PREFIX';/" "$MAGENTO_SOURCE/wp-config.php"
+	# see http://devdocs.magento.com/guides/v2.1/install-gde/install/cli/install-cli-install.html
+	set_var_if_null 'MAGENTO_ADMIN_USER' 'admin'
+	set_var_if_null 'MAGENTO_ADMIN_PASSWORD' 'MS173m_QN'
+	set_var_if_null 'MAGENTO_ADMIN_FIRSTNAME' 'firstname'
+	set_var_if_null 'MAGENTO_ADMIN_LASTNAME' 'lastname'
+	set_var_if_null 'MAGENTO_ADMIN_EMAIL' 'admin@example.com'
+	set_var_if_null 'MAGENTO_BACKEND_FRONTNAME' 'admin_1qn'
+
+	set_var_if_null 'MAGENTO_DB_HOST' '127.0.0.1'
+	set_var_if_null 'MAGENTO_DB_NAME' 'magento'
+	set_var_if_null 'MAGENTO_DB_USERNAME' 'magento'
+	set_var_if_null 'MAGENTO_DB_PASSWORD' 'MS173m_QN'
+	set_var_if_null 'MAGENTO_DB_PREFIX' 'm2_'
+	
+	# replace {'localhost',,} with '127.0.0.1'
+	if [ "${MAGENTO_DB_HOST,,}" = "localhost" ]; then
+		export MAGENTO_DB_HOST="127.0.0.1"
+		echo "Replace localhost with 127.0.0.1 ... $MAGENTO_DB_HOST"
+	fi       	
 
 	# Because Azure Web App on Linux uses /home/site/wwwroot,
 	# so if /home/site/wwwroot doesn't exist, 
 	# we think the container is not running on Auzre.
 	if [ ! -d "$MAGENTO_HOME_AZURE" ]; then
-        	rm -rf $MAGENTO_HOME && mkdir -p $MAGENTO_HOME
+		echo "INFO: $MAGENTO_HOME_AZURE not found!"
+		rm -rf $MAGENTO_HOME && mkdir -p $MAGENTO_HOME
 		rm -rf $PHPMYADMIN_HOME && mkdir -p $PHPMYADMIN_HOME
 		rm -rf $MARIADB_DATA_DIR && mkdir -p $MARIADB_DATA_DIR
 		rm -rf $HTTPD_LOG_DIR && mkdir -p $HTTPD_LOG_DIR
@@ -46,15 +47,16 @@ if [ ! -f "$MAGENTO_HOME/wp-config.php" ]; then
 		test ! -d $HTTPD_LOG_DIR_AZURE && mkdir -p $HTTPD_LOG_DIR_AZURE
 		test ! -d $MARIADB_LOG_DIR_AZURE && mkdir -p $MARIADB_LOG_DIR_AZURE
 	fi
-        cp -R $MAGENTO_SOURCE/* $MAGENTO_HOME/ && chown -R www-data:www-data $MAGENTO_HOME/ && rm -rf $MAGENTO_SOURCE
-        cp -R $PHPMYADMIN_SOURCE/* $PHPMYADMIN_HOME/ && chown -R www-data:www-data $PHPMYADMIN_HOME/ && rm -rf $PHPMYADMIN_SOURCE
-        cp -R $MARIADB_DATA_DIR_TEMP/* $MARIADB_DATA_DIR/ && chown -R mysql:mysql $MARIADB_DATA_DIR/ && rm -rf $MARIADB_DATA_DIR_TEMP
+	
+	cp -R $PHPMYADMIN_SOURCE/. $PHPMYADMIN_HOME/ && chown -R www-data:www-data $PHPMYADMIN_HOME/ && rm -rf $PHPMYADMIN_SOURCE
+	cp -R $MARIADB_DATA_DIR_TEMP/. $MARIADB_DATA_DIR/ && chown -R mysql:mysql $MARIADB_DATA_DIR/ && rm -rf $MARIADB_DATA_DIR_TEMP
 	chown -R www-data:www-data $HTTPD_LOG_DIR/
 	chown -R mysql:mysql $MARIADB_LOG_DIR/
 
 	# check if use native MariaDB
 	# if yes, we allow users to use native phpMyAdmin and native Redis server
 	if [ $MAGENTO_DB_HOST = "127.0.0.1" ]; then
+		echo "using $MAGENTO_DB_HOST as database host ..."
 		# set vars for phpMyAdmin if not provided
 		set_var_if_null 'PHPMYADMIN_USERNAME' 'phpmyadmin'
 		set_var_if_null 'PHPMYADMIN_PASSWORD' 'MS173m_QN'
@@ -67,8 +69,37 @@ if [ ! -f "$MAGENTO_HOME/wp-config.php" ]; then
 		# start native Redis server
 		redis-server --daemonize yes
 	fi
+
+	echo "copying Magento source files to $MAGENTO_HOME ..."
+	cp -R $MAGENTO_SOURCE/. $MAGENTO_HOME/ && rm -rf $MAGENTO_SOURCE	
+
+	$MAGENTO_HOME/bin/magento setup:install \
+		--admin-user=$MAGENTO_ADMIN_USER \
+		--admin-password=$MAGENTO_ADMIN_PASSWORD \
+		--admin-firstname=$MAGENTO_ADMIN_FIRSTNAME \
+		--admin-lastname=$MAGENTO_ADMIN_LASTNAME \
+		--admin-email=$MAGENTO_ADMIN_EMAIL \
+		--admin-use-security-key=1 \
+		--backend-frontname=$MAGENTO_BACKEND_FRONTNAME \
+		--db-host=$MAGENTO_DB_HOST \
+		--db-name=$MAGENTO_DB_NAME \
+		--db-user=$MAGENTO_DB_USERNAME \
+		--db-password=$MAGENTO_DB_PASSWORD \
+		--db-prefix=$MAGENTO_DB_PREFIX \
+		--use-rewrites=1 \
+		--base-url=$MAGENTO_BASE_URL
+
+	if [ -f $MAGENTO_HOME/app/etc/env.php ]; then
+		echo "switching to PRODUCTION mode..."
+		$MAGENTO_HOME/bin/magento deploy:mode:set production		
+	fi
+
+	chown -R www-data:www-data $MAGENTO_HOME/
+	find $MAGENTO_HOME/var $MAGENTO_HOME/vendor $MAGENTO_HOME/pub/static $MAGENTO_HOME/pub/media $MAGENTO_HOME/app/etc -type d -exec chmod g+ws {} \;
+	find $MAGENTO_HOME/var $MAGENTO_HOME/vendor $MAGENTO_HOME/pub/static $MAGENTO_HOME/pub/media $MAGENTO_HOME/app/etc -type f -exec chmod g+w {} \;
+
 else
-	if grep "connectstr_dbhost = '127.0.0.1'" "$MAGENTO_HOME/wp-config.php"; then
+	if grep "'host' => '127.0.0.1'" "$MAGENTO_HOME/app/etc/env.php"; then
 		service mysql start
 		redis-server --daemonize yes
 	fi
