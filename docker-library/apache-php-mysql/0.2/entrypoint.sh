d1c865813bf18eb744904812aa5ca609689e2022
LeonZhang77 <zhangjianfeng73@hotmail.com>
2017-08-15 15:30:23 +0800

    remove entrypoint.log (#71)

--- docker-library/apache-php-mysql/0.2/entrypoint.sh
+++ docker-library/apache-php-mysql/0.2/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-log(){
-	while read line ; do
-		echo "`date '+%D %T'` $line"
-	done
-}
 
 set -e
-logfile=/home/LogFiles/entrypoint.log
-test ! -f $logfile && mkdir -p /home/LogFiles && touch $logfile
-exec > >(log | tee -ai $logfile)
-exec 2>&1
 
 set_var_if_null(){
 	local varname="$1"
